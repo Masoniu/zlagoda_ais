fa2f099c35cf18c42b9d27fccc36f14c389f2956
Serheiev Maksym <01022019serf@gmail.com>
2026-04-17 01:43:28 +0300

    succesfully pass the login POST query

--- backend/core/security.py
+++ backend/core/security.py
@@ -7,7 +7,7 @@ ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
