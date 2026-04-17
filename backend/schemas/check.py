3fa6b1eee64a99ddf0f4b23770a435ea13079516
Serheiev Maksym <01022019serf@gmail.com>
2026-04-17 12:30:09 +0300

    add check schema

---
+++ backend/schemas/check.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+from decimal import Decimal
+
+class SaleItemCreate(BaseModel):
+    UPC: str = Field(..., max_length=12)
+    product_number: int = Field(..., gt=0)
+
+class CheckCreate(BaseModel):
+    check_number: str = Field(..., max_length=10)
+    card_number: Optional[str] = Field(None, max_length=13)
+    items: List[SaleItemCreate]
+
+class CheckResponse(BaseModel):
+    check_number: str
+    id_employee: str
+    card_number: Optional[str]
+    print_date: datetime
+    sum_total: Decimal
+    vat: Decimal
+
+    class Config:
+        from_attributes = True
