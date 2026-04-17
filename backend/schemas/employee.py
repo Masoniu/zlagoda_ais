ff47ee1ece27e31b259426a0b751c6f6b76f37b5
Roman Marysyk <romamarysyk@gmail.com>
2026-04-17 18:56:41 +0300

    refactor employee schemas to improve structure and add support for create and update operations

--- backend/schemas/employee.py
+++ backend/schemas/employee.py
@@ -2,19 +2,32 @@ from pydantic import BaseModel, Field
 from datetime import date
 from typing import Optional
 
-class EmployeeLogin(BaseModel):
+class EmployeeBase(BaseModel):
+    empl_surname: str = Field(..., max_length=50)
+    empl_name: str = Field(..., max_length=50)
+    empl_patronymic: Optional[str] = Field(None, max_length=50)
+    empl_role: str = Field(..., max_length=50)
+    salary: float
+    date_of_start: date
+    date_of_birth: date
+    phone_number: str = Field(..., max_length=13)
+    city: Optional[str] = Field(None, max_length=50)
+    street: Optional[str] = Field(None, max_length=50)
+    zip_code: Optional[str] = Field(None, max_length=9)
+
+class EmployeeCreate(EmployeeBase):
     id_employee: str = Field(..., min_length=1, max_length=10)
     password: str = Field(..., min_length=4)
 
-class EmployeeResponse(BaseModel):
+class EmployeeUpdate(EmployeeBase):
+    pass
+
+class EmployeeResponse(EmployeeBase):
     id_employee: str
-    empl_surname: str
-    empl_name: str
-    empl_patronymic: Optional[str] = None
-    empl_role: str
-    salary: float
-    date_of_start: date
-    phone_number: str
 
     class Config:
         from_attributes = True
+
+class EmployeeLogin(BaseModel):
+    id_employee: str = Field(..., min_length=1, max_length=10)
+    password: str = Field(..., min_length=4)
