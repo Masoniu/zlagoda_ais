30790467f8de3589b24d6e8f7770464b29c25027
Roman Marysyk <romamarysyk@gmail.com>
2026-04-18 19:35:44 +0300

    add automated cleanup of checks older than 3 years

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,48 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from backend.api.auth import router as auth_router
 from backend.api.categories import router as categories_router
 from backend.api.products import router as products_router
 from backend.api.store_product import router as store_products_router
 from backend.api.customer_cards import router as customer_cards_router
 from backend.api.checks import router as checks_router
-from contextlib import asynccontextmanager
-from backend.core.database import create_db_pool
 from backend.api.employees import router as employees_router
-# uvicorn backend.main:app --reload
+from backend.core.database import create_db_pool
+
+
+async def cleanup_old_checks(pool):
+    while True:
+        try:
+            async with pool.acquire() as conn:
+                #видалення чеків через 3 роки
+                result = await conn.execute('''
+                                            DELETE
+                                            FROM "check"
+                                            WHERE print_date < NOW() - INTERVAL '3 years'
+                                            ''')
+                print(f"Автоочищення: {result}")
+        except Exception as e:
+            print(f"Помилка очищення чеків: {e}")
+
+        # Очікування 24 години (86400 секунд)
+        await asyncio.sleep(86400)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.pool = await create_db_pool()
+    cleanup_task = asyncio.create_task(cleanup_old_checks(app.state.pool))
     yield
+
+    cleanup_task.cancel()
+    try:
+        await cleanup_task
+    except asyncio.CancelledError:
+        pass
     await app.state.pool.close()
+
 app = FastAPI(title="ZLAGODA AIS API", lifespan=lifespan)
 
 app.add_middleware(
